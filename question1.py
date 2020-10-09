1f349dcd37608660ab971ceca888e08067f80a65
Jeremy Ortega <jeremy.ortega@daviestech.org>
2020-10-09 00:28:55 +0000

    finished

---
+++ question1.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a integer: "))
+
+print(num+1)
+print(num+2)
+print(num+3)
---
+++ question2.py
@@ -0,0 +1,5 @@
+x = float(input("Enter an integer: "))
+y = float(input("Enter an integer: "))
+z = float(input("Enter an integer: "))
+
+print(x+y+z)
